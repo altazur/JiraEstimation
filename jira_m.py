48be806bd7df0fc2b3546eddaf96d026e4191818
Анатолий Ромса < @web100-krg.local>
2019-10-11 16:11:25 +0300

    Fix get_dev_estimate_precision() bug when previous data influences the next

--- jira_m.py
+++ jira_m.py
@@ -18,11 +18,11 @@ def get_dev_estimate_precision(url, login_name, login_pwd, developer_names_list)
 	fot_project_id = fot_project.id
 	#Get last release id from the porject
 	last_release_id = fot_project.raw['versions'][len(fot_project.raw['versions'])-1]['id']
-	original_estimate_sum = 0
-	timespent_sum=0
 	#Search returns 50 items. maxResult arg can exceed it. For now we don't use maxResult cuz 50 is more than enough
 	developer_precision={}
 	for developer_name in developer_names_list:
+		original_estimate_sum = 0
+		timespent_sum = 0
 		try:
 			developer_issues = jiraAPI.search_issues("project ="+fot_project_id+" AND fixVersion="+last_release_id+" AND assignee='"+developer_name+"'")
 		except NameError:
@@ -33,7 +33,7 @@ def get_dev_estimate_precision(url, login_name, login_pwd, developer_names_list)
 			original_estimate_sum += issue.fields.timeoriginalestimate or 0
 			timespent_sum += issue.fields.timespent or 0
 		try:
-			yield (timespent_sum/original_estimate_sum, developer_name)
+			yield (timespent_sum/original_estimate_sum if (timespent_sum is not 0 and original_estimate_sum is not 0) else 0, developer_name)
 		except ZeroDivisionError:
 			tb = traceback.format_exc()
 			return 0
